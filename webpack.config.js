266fa4da38945b26eb74c101265f72decb9a160c
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-05-15 21:30:15 +0200

    web

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+        output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js'
+    },
+    module: {
+       rules: [
+    {
+        test: /\.js$/,
+        loader: "babel-loader"
+    },
+    {
+        test: /\.css$/,
+        use: [
+            { loader: 'style-loader'},
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: true
+                }
+            }   
+        ]
+    }
+]
