38a94bbaea5c7190fc9819bd903b0604bcb7e713
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-05-22 12:40:07 +0200

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,36 @@
 ﻿import React from 'react';
+import uuid from 'uuid';
+import style from './style.css';
+import Title from './Title';
 
 class App extends React.Component {
-    constructor(props) {
+    constructor(props){
         super(props);
         this.state = {
             data: []
         };
     }
-    addTodo(val) {
+    addTodo(val){
         const todo = {
             text: val,
             id: uuid.v4(),
         };
         const data = [...this.state.data, todo];
-        this.setState({
-            data
-        });
+        this.setState({data});
     }
     removeTodo(id) {
         const remainder = this.state.data.filter(todo => todo.id !== id);
-        this.setState({
-            data: remainder
-        });
+        this.setState({data: remainder});
     }
+
     render() {
         return (
             <div className={style.TodoApp}>
-                Tutaj pojawią się komponenty naszej aplikacji.
+                <Title title='Tytuł aplikacji' length={this.state.data.length} />
+                // Tu pojawią się komponenty aplikacj
             </div>
         );
     }
-    .TodoApp {
-    background-color: #F5F5F5;
-    color: #222;
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    width: 100vw;
-    height: 100vh;
-}
 }
 
 export default App;
-
